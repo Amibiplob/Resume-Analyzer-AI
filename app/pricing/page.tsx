b8f48dd2bf603a4c4515cf7c2f2b57ec24815ff3
AmiBiplob <muhammadbiplob7@gmail.com>
2026-05-12 20:06:19 +0600

    pricing page added

---
+++ app/pricing/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import PricingSection from "@/components/home/PricingSection";
+
+export default function PricingPage() {
+  return (
+    <>
+      <Navbar />
+      <PricingSection />
+      <Footer />
+    </>
+  );
+}
