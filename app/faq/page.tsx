b039ce6a544de5761531751987e8dc35ba7cabfc
AmiBiplob <muhammadbiplob7@gmail.com>
2026-05-12 23:30:23 +0600

    faq page added

---
+++ app/faq/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import FaqSection from "@/components/home/FaqSection";
+
+export default function FaqPage() {
+  return (
+    <>
+      <Navbar />
+      <FaqSection />
+      <Footer />
+    </>
+  );
+}
